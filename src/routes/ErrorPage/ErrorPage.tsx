cb3baddc11e505921ac43b4b9dfba7f5f2d4870c
Roman <mamontov.r1995@gmail.com>
2023-03-13 19:09:21 +0300

    feat: add styles to error page & back button

--- src/routes/ErrorPage/ErrorPage.tsx
+++ src/routes/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,21 @@
-// import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import styles from './ErrorPage.module.css'
 
 const ErrorPage = () => {
+  const navigate = useNavigate()
   return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p></p>
-    </div>
+    <main className={styles.errorPage}>
+      <div className={styles.container}>
+        <p className={styles.status}>404</p>
+        <h1 className={styles.title}>Page not found</h1>
+        <p className={styles.describe}>Sorry, we couldn’t find the page you’re looking for.</p>
+        <div className={styles.buttonContainer}>
+          <button onClick={() => navigate(-1)} className={styles.button}>
+            Go back
+          </button>
+        </div>
+      </div>
+    </main>
   )
 }
 
