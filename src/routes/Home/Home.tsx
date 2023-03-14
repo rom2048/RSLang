9ae10de9de53a61ea2a1b9e38c3eb5bc7574a8dd
Roman <mamontov.r1995@gmail.com>
2023-03-14 16:00:43 +0300

    refactor: section component

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,11 +1,18 @@
 import About from '../../components/About/About'
+import CardList from '../../components/CardList/CardList'
 import Hero from '../../components/Hero/Hero'
+import Section from '../../components/Section/Section'
 
 const Home = () => {
   return (
     <main>
       <Hero />
-      <About />
+      <Section title='Card List'>
+        <CardList></CardList>
+      </Section>
+      <Section title='About'>
+        <About />
+      </Section>
     </main>
   )
 }
